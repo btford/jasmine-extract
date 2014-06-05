8fb4ef1fc687fffdd28785f65d78251c8871263d
Brian Ford <btford@umich.edu>
2014-06-04 22:22:39 -0700

    chore: reword specs

--- test/basic.spec.js
+++ test/basic.spec.js
@@ -3,32 +3,32 @@ var fs      = require('fs');
 var extract = require('../extract');
 
 describe('extract', function () {
-  it('should work with a simple spec', function () {
+  it('should parse a simple spec', function () {
     var spec = parseAndExtract('basic.js');
     expect(keys(spec.describes)).toEqual(['foo']);
     expect(keys(spec.describes.foo.its)).toEqual(['should have x equal y']);
   });
 
-  it('should work with nested describes', function () {
+  it('should extract nested describes', function () {
     var spec = parseAndExtract('nested-describe.js');
     expect(keys(spec.describes)).toEqual(['foo']);
     expect(keys(spec.describes.foo.describes)).toEqual(['bar']);
   });
 
-  it('should work with beforeEach blocks', function () {
+  it('should extract beforeEach blocks', function () {
     var spec = parseAndExtract('before-each.js');
     expect(keys(spec.describes)).toEqual(['foo']);
     expect(keys(spec.describes.foo.its)).toEqual(['should be great']);
     expect(spec.describes.foo.beforeEach.length).toBe(1);
   });
 
-  it('should work with afterEach blocks', function () {
+  it('should extract afterEach blocks', function () {
     var spec = parseAndExtract('after-each.js');
     expect(keys(spec.describes)).toEqual(['foo']);
     expect(spec.describes.foo.afterEach.length).toBe(1);
   });
 
-  it('extract statements before jasmine blocks', function () {
+  it('should extract statements before jasmine blocks', function () {
     var spec = parseAndExtract('before.js');
     expect(spec.before.length).toBe(1);
   });
